c8765ba31a85bad50c1d5f5efed44f431cfc1e2d
nazariosafariesqi <nazariotyo@gmail.com>
2021-04-23 20:56:52 +0700

    Praktikum 1

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return array
+     */
+    abstract public function rules();
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response :: HTTP_UNAUTHORIZED
+        ));
+    }
+}
